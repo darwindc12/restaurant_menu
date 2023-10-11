b91bc7e14bb3e38e83b8aca446c18bbef3131c99
darwin <darwindelacruz0012@gmail.com>
2023-10-11 16:57:44 +0800

    Create index1 html file, used for testing to retrieve data from model to views to template

--- restuarant_menu/views.py
+++ restuarant_menu/views.py
@@ -4,12 +4,14 @@ from .models import Item, MEAL_TYPE
 
 
 class MenuList(generic.ListView):
+    model = Item
     queryset = Item.objects.order_by("-date_created")
-    template_name = "index.html"
+    template_name = "index1.html"
+    context_object_name = 'data_list'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['meals'] = MEAL_TYPE
+        # context['meals'] = Item.description
         return context
 
 
